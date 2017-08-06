5a4decf14e41ac30641290e599a9dc240fe73309
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-07 01:30:41 +0900

    NullPointerException: Cannot get property 'ssh' on null objectに苦しむ

--- vars/sshCmd.groovy
+++ vars/sshCmd.groovy
@@ -6,6 +6,7 @@ def call(server, cmd)
     // 宣言
     def stdout = new StringBuffer(), stderror = new StringBuffer()
 
+    try {
     // 設定ファイルロード
     try {
         def config = ['path':'/opt/app/conf', 'file':'env.groovy']
@@ -28,6 +29,11 @@ def call(server, cmd)
 
     // コマンド結果出力が完了するまで待機
     proc.waitForProcessOutput()
+
+    } catch(Exception e) {
+        println 'failed'
+        throw e
+    }
 
     // 実行結果を返却
     def result = ['stdout':stdout, 'stderror':stderror]
