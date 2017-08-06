4dbf7ea347e30f0910ced45d7b7b79960247c44d
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-07 00:40:28 +0900

    NullPointerException: Cannot get property 'ssh' on null objectに苦しむ

--- vars/sshCmd.groovy
+++ vars/sshCmd.groovy
@@ -21,23 +21,23 @@ def call(server, cmd)
     // 実行結果を返却
     def result = ['stdout':stdout, 'stderror':stderror]
     return result
+}
 
-    def make_cmd(server, cmd)
-    {
-        // 設定ファイルロード
-        try {
-            def config = ['path':'/opt/app/conf', 'file':'env.groovy']
-            def job = new ConfigSlurper().parse(new File(config['path'] + "/" + config['file']).toURL())
-        } catch(Exception e) {}
+def make_cmd(server, cmd)
+{
+    // 設定ファイルロード
+    try {
+        def config = ['path':'/opt/app/conf', 'file':'env.groovy']
+        def job = new ConfigSlurper().parse(new File(config['path'] + "/" + config['file']).toURL())
+    } catch(Exception e) {}
 
-        // コマンド組立
-        def ssh_cmd = [
-            ('ssh -i ' + env.session.ssh."${server}".identity),
-            ('-p '     + env.session.ssh."${server}".port),
-            (            env.session.ssh."${server}".user + '@' + env.session.ssh."${server}".host),
-            (cmd)
-        ].join(' ')
+    // コマンド組立
+    def ssh_cmd = [
+        ('ssh -i ' + env.session.ssh."${server}".identity),
+        ('-p '     + env.session.ssh."${server}".port),
+        (            env.session.ssh."${server}".user + '@' + env.session.ssh."${server}".host),
+        (cmd)
+    ].join(' ')
 
-        return ssh_cmd
-    }
+    return ssh_cmd
 }
