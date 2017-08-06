0d8de2639877ac4c8b800830a57d2857c4d7cf11
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-07 00:32:56 +0900

    NullPointerException: Cannot get property 'ssh' on null objectに苦しむ

--- vars/sshCmd.groovy
+++ vars/sshCmd.groovy
@@ -6,21 +6,8 @@ def call(server, cmd)
     // 宣言
     def stdout = new StringBuffer(), stderror = new StringBuffer()
 
-    // 設定ファイルロード
-    //try {
-    //    def config = ['path':'/opt/app/conf', 'file':'env.groovy']
-    //    def job = new ConfigSlurper().parse(new File(config['path'] + "/" + config['file']).toURL())
-    //} catch(Exception e) {}
-
-    println env.ssesion.ssh.webap.identity
-    println env.ssesion.ssh."${server}".identity
-    // コマンド組立
-    def ssh_cmd = [
-        ('ssh -i ' + env.session.ssh."${server}".identity),
-        ('-p '     + env.session.ssh."${server}".port),
-        (            env.session.ssh."${server}".user + '@' + env.session.ssh."${server}".host),
-        (cmd)
-    ].join(' ')
+    //
+    make_cmd(server, cmd)
 
     // コマンド実行
     def proc = ssh_cmd.execute()
@@ -34,4 +21,23 @@ def call(server, cmd)
     // 実行結果を返却
     def result = ['stdout':stdout, 'stderror':stderror]
     return result
+
+    def make_cmd(server, cmd)
+    {
+        // 設定ファイルロード
+        try {
+            def config = ['path':'/opt/app/conf', 'file':'env.groovy']
+            def job = new ConfigSlurper().parse(new File(config['path'] + "/" + config['file']).toURL())
+        } catch(Exception e) {}
+
+        // コマンド組立
+        def ssh_cmd = [
+            ('ssh -i ' + env.session.ssh."${server}".identity),
+            ('-p '     + env.session.ssh."${server}".port),
+            (            env.session.ssh."${server}".user + '@' + env.session.ssh."${server}".host),
+            (cmd)
+        ].join(' ')
+
+        return ssh_cmd
+    }
 }
