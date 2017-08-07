2eea8c8144258f3edafa596b98c012bb18244de4
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-07 23:51:44 +0900

    NullPointerException: Cannot get property 'ssh' on null object治らない

--- vars/loadEnvConfig.groovy
+++ vars/loadEnvConfig.groovy
@@ -6,10 +6,32 @@ def call(env)
     // nonserializable対策
     try
     {
-        def config = ['path':'/opt/app/conf', 'file':'env.groovy']
-        env = new ConfigSlurper().parse(new File(config['path'] + "/" + config['file']).toURL())
+        //def config = ['path':'/opt/app/conf', 'file':'env.groovy']
+        //env = new ConfigSlurper().parse(new File(config['path'] + "/" + config['file']).toURL())
+
+    env.with {
+
+       session { ssh {
+          // webapサーバ
+          webap.host     = '127.0.0.1'
+          webap.port     = 22
+          webap.user     = 'redmine'
+          webap.password = 'redmine'
+          webap.identity = System.properties.'user.home' + '/.ssh/id_rsa'
+          // batchサーバ
+          batch.host     = '127.0.0.1'
+          batch.port     = 22
+          batch.user     = 'redmine'
+          batch.password = 'redmine'
+          batch.identity = System.properties.'user.home' + '/.ssh/id_rsa'
+       }}
     }
-    catch(Exception e) {}
+    }
+    catch(Exception e) {
+
+    	println 'env: failed'
+    	throw e
+    }
 
     return env
 }
