021738cba23daacd7cb630ab2fe30590710fd7e6
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-08 06:13:10 +0900

    設定ロードメソッド、スコープ宣言修正

--- vars/sshCmd.groovy
+++ vars/sshCmd.groovy
@@ -5,30 +5,29 @@ def call(env, server, cmd)
 {
     def stdout = new StringBuffer(), stderror = new StringBuffer()
 
-    try
+    // コマンド組立
+    def ssh_cmd = [
+        ('ssh -i ' + env.session.ssh."${server}".identity),
+        ('-p '     + env.session.ssh."${server}".port),
+        (            env.session.ssh."${server}".user + '@' + env.session.ssh."${server}".host),
+        (cmd)
+    ].join(' ')
+
+    // コマンド実行
+    def proc = ssh_cmd.execute()
+
+    // コマンド結果保存先設定
+    proc.consumeProcessOutput(stdout, stderror)
+
+    // コマンド結果出力が完了するまで待機
+    proc.waitForProcessOutput()
+
+    // 標準エラー出力が発生している場合は、標準エラー出力も返却する
+    if(stderror.size() == 0)
     {
-        // コマンド組立
-        def ssh_cmd = [
-            ('ssh -i ' + env.session.ssh."${server}".identity),
-            ('-p '     + env.session.ssh."${server}".port),
-            (            env.session.ssh."${server}".user + '@' + env.session.ssh."${server}".host),
-            (cmd)
-        ].join(' ')
-
-        // コマンド実行
-        def proc = ssh_cmd.execute()
-
-        // コマンド結果保存先設定
-        proc.consumeProcessOutput(stdout, stderror)
-
-        // コマンド結果出力が完了するまで待機
-        proc.waitForProcessOutput()
+        return stdout
     }
-    catch(Exception e) {
-
-        println 'failed'
-        throw e
+    else {
+        return [stdout, stderror]
     }
-
-    return [stdout, stderror]
 }
