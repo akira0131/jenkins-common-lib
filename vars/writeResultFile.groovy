a72044fd692d7af67ee67aaf107f83cc31d6d21a
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-10 03:07:56 +0900

    変数をファイルに書き込むメソッドテストケース3を追加

--- vars/writeResultFile.groovy
+++ vars/writeResultFile.groovy
@@ -1,9 +1,20 @@
-#!/usr/bin/groovy
+#!groovy
 
 // 変数をファイルに書き込むメソッド
 def call(data, filename)
 {
+    // 書込対象のファイル
     def temp = ['path':'/opt/var/jenkins/temp', 'file':'result_' + filename + '.yml']
+
+    // ファイルが既に存在していた場合は削除する
+    File f = new File(temp['path'] + "/" + temp['file'])
+    if(f.exists())
+    {
+        f.delete()
+        printMsg ('info', '書込対象のファイルが既に存在しているため、削除しました。')
+    }
+
+    // Yaml形式で書込
     writeYaml(
         file: temp['path'] + "/" + temp['file'],
         data: data
