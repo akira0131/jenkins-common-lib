875d732f82b0393c1f9f31102391f0095f188844
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-11 19:55:01 +0900

    MANUAL用ジョブ修正

--- vars/writeDataToFile.groovy
+++ vars/writeDataToFile.groovy
@@ -1,10 +1,14 @@
 #!groovy
 
-// 変数をファイルに書き込むメソッド
+// 変数をファイルに書き出すメソッド
 def call(data, filename)
 {
     // 書込対象のファイル
-    def temp = ['path':'/opt/var/jenkins/temp', 'file':'result_' + filename + '.yml']
+    def temp = ['path':'/opt/var/jenkins/temp', 'file': filename + '.yml']
+
+    printMsg('info', 'データをファイルに出力します。')
+    printMsg('info', '出力形式: YAML')
+    printMsg('info', '出力先: ' + temp['path'] + "/" + temp['file'])
 
     // ファイルが既に存在していた場合は削除する
     File f = new File(temp['path'] + "/" + temp['file'])
