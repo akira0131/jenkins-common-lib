913d8fd0daf13e3c4e63bc1ee24c88b77760ad98
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-14 06:01:38 +0900

    マニュアル動作対応

--- vars/WriteBufferToFile.groovy
+++ vars/WriteBufferToFile.groovy
@@ -1,5 +1,8 @@
 #!groovy
 
+import java.io.File.write;
+import java.io.BufferedWriter;
+
 // 変数をファイルに書き出すメソッド
 def call(data, fileName)
 {
@@ -8,5 +11,6 @@ def call(data, fileName)
     temp = ['path':'/opt/var/jenkins/temp', 'file': 'result_' + fileName + '.log']
     printMsg('info', '標準出力結果出力先: ' + temp['path'] + "/" + temp['file'])
     File f = new File(temp['path'] + "/" + temp['file'])
-    f.write(data)
+    BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
+    bw.write(data)
 }
