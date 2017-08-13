d9f7343ac1946b12d0ef7e08de99c80c847ae4c0
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-14 05:03:59 +0900

    マニュアル動作対応

--- vars/sshCmd.groovy
+++ vars/sshCmd.groovy
@@ -6,20 +6,15 @@ def call(server, cmd)
     def stdout = new StringBuffer(), stderror = new StringBuffer()
 
     // AP設定ロード
-    config = loadAppConfig()
+    def config = loadAppConfig()
 
     // コマンド組立
-    ssh_cmd = [
+    def ssh_cmd = [
         ('ssh -i ' + config.session.ssh."${server}".identity),
         ('-p '     + config.session.ssh."${server}".port),
         (            config.session.ssh."${server}".user + '@' + config.session.ssh."${server}".host),
         (cmd)
     ].join(' ')
-
-    // 開始メッセージ
-    printMsg('info', 'SSHコマンドを実行します。')
-    printMsg('info', 'Command Param: server = ' + server)
-    printMsg('info', 'Command Param: command = ' + cmd)
 
     // コマンド実行
     def proc = ssh_cmd.execute()
@@ -30,36 +25,12 @@ def call(server, cmd)
     // コマンド結果出力が完了するまで待機
     proc.waitForProcessOutput()
 
-    // コマンド結果をファイルに出力
-    printMsg('info', 'コマンド結果をファイルに出力します。')
-
-    //if(stderror.size() == 0)
-    //{
-        // 標準出力
-        //temp = ['path':'/opt/var/jenkins/temp', 'file': 'result_ssh_cmd_wu_execute_cnt_stdout_' + server + '.log']
-        //printMsg('info', '標準出力結果出力先: ' + temp['path'] + "/" + temp['file'])
-        //WriteBufferToFile(stdout,temp['path'] + "/" + temp['file'])
-    //}
-    //else
-    //{
-        // 標準出力
-        //temp = ['path':'/opt/var/jenkins/temp', 'file': 'result_ssh_cmd_wu_execute_cnt_stdout_' + server + '.log']
-        //printMsg('info', '標準出力結果出力先: ' + temp['path'] + "/" + temp['file'])
-        //WriteBufferToFile(stdout,temp['path'] + "/" + temp['file'])
-
-        // 標準エラー出力
-        //temp = ['path':'/opt/var/jenkins/temp', 'file': 'result_ssh_cmd_wu_execute_cnt_stderror_' + server + '.log']
-        //printMsg('info', '標準エラー出力結果出力先: ' + temp['path'] + "/" + temp['file'])
-        //WriteBufferToFile(stderror,temp['path'] + "/" + temp['file'])
-    //}
-
-    // コマンド結果を返却
+    // 標準エラーが発生している場合は、標準エラーも返却する
     if(stderror.size() == 0)
     {
         return ['stdout':stdout]
     }
-    else
-    {
+    else {
         return ['stdout':stdout, 'stderror':stderror]
     }
 }
