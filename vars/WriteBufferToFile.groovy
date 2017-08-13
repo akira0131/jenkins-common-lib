ad0273c09c101506416f38f17f9980835a370f26
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-14 06:04:17 +0900

    マニュアル動作対応

--- vars/WriteBufferToFile.groovy
+++ vars/WriteBufferToFile.groovy
@@ -1,16 +1,22 @@
 #!groovy
 
-import java.io.File.write;
-import java.io.BufferedWriter;
-
 // 変数をファイルに書き出すメソッド
 def call(data, fileName)
 {
-    printMsg('info', 'コマンド結果をファイルに出力します。')
-
+    // 書込対象のファイル
     temp = ['path':'/opt/var/jenkins/temp', 'file': 'result_' + fileName + '.log']
+
+    printMsg('info', 'コマンド結果をファイルに出力します。')
     printMsg('info', '標準出力結果出力先: ' + temp['path'] + "/" + temp['file'])
+
+    // ファイルが既に存在していた場合は削除する
     File f = new File(temp['path'] + "/" + temp['file'])
+    if(f.exists())
+    {
+        f.delete()
+        printMsg ('info', '書込対象のファイルが既に存在しているため、削除しました。')
+    }
+
     BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
     bw.write(data)
 }
