213c8b3bd1a3d4b98f1b011918bb8d61eee0dacf
竹内 晃 <bluesea.ar131@docomo.ne.jp>
2017-08-14 22:24:20 +0900

    bufferwriter調整

--- vars/sshCmd.groovy
+++ vars/sshCmd.groovy
@@ -30,15 +30,19 @@ def call(server, cmd)
     // コマンド結果の出力が完了するまで待機
     proc.waitForProcessOutput()
 
-    // コマンド結果の標準出力を書き出すインスタンス生成
-    BufferedWriter bwout = new BufferedWriter(new FileWriter(new File('/opt/var/jenkins/temp/ssh_cmd_stdout.txt')))
+    // コマンド結果をファイルに書き出す
+    writeBufferToFile()
 
-    // コマンド結果の標準エラー出力を書き出すインスタンス生成
-    BufferedWriter bwerror = new BufferedWriter(new FileWriter(new File('/opt/var/jenkins/temp/ssh_cmd_stderror.txt')))
+}
 
+def writeBufferToFile()
+{
     // 標準エラーが発生している場合は、標準エラーもファイルに書き出す
     if(stderror.size() == 0)
     {
+        // コマンド結果の標準出力を書き出すインスタンス生成
+        BufferedWriter bwout = new BufferedWriter(new FileWriter(new File('/opt/var/jenkins/temp/ssh_cmd_stdout.txt')))
+
         // 書き出すデータをセット
         bwout.write(stdout.toString())
 
@@ -51,6 +55,10 @@ def call(server, cmd)
     }
     else
     {
+        // コマンド結果を書き出すインスタンス生成
+        BufferedWriter bwout = new BufferedWriter(new FileWriter(new File('/opt/var/jenkins/temp/ssh_cmd_stdout.txt')))
+        BufferedWriter bwerror = new BufferedWriter(new FileWriter(new File('/opt/var/jenkins/temp/ssh_cmd_stderror.txt')))
+
         // 書き出すデータをセット
         bwout.write(stdout.toString())
         bwerror.write(stderror.toString())
